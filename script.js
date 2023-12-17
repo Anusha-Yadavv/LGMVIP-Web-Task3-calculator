1c8f37e98aff3d50a7914ca729786784e1ccee4b
Anusha-Yadavv <anushakottam22@gmail.com>
2023-12-17 20:01:33 +0530

    Writing logic for every operator

---
+++ script.js
@@ -0,0 +1,49 @@
+let outputContainer = document.getElementById("output_container");
+let currentInput = "";
+
+function clearOutput() {
+  currentInput = "";
+  console.log("current input", currentInput);
+  updateOutput();
+}
+
+function appendSymbol(symbol) {
+  currentInput += symbol;
+  console.log("current input", currentInput);
+
+  updateOutput();
+}
+
+function appendNumber(number) {
+  currentInput += number;
+  console.log("current input", currentInput);
+
+  updateOutput();
+}
+
+function appendDecimal() {
+  if (!currentInput.includes(".")) {
+    currentInput += ".";
+    console.log("current input", currentInput);
+
+    updateOutput();
+  }
+}
+
+function calculate() {
+  try {
+    currentInput = eval(currentInput).toString();
+    console.log("current input", currentInput);
+
+    updateOutput();
+  } catch (error) {
+    currentInput = alert("Offoo!! Please enter valid input");
+    console.log("current input", currentInput);
+
+    updateOutput();
+  }
+}
+
+function updateOutput() {
+  outputContainer.textContent = currentInput;
+}
